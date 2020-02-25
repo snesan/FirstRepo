c42967a59a77ea8fabd068d067a5c9f0a36a52f9
Vidyasagar Nesanuru <sagar.nesanuru@gmail.com>
2020-02-25 22:12:09 +0530

    Pandas
    Pandas practice excercise

---
+++ Pandas.py
@@ -0,0 +1,8 @@
+## Pandas practice script
+import pandas as pd
+import numpy as np
+### read the movies dataset
+movie = pd.read_csv("IMDB-Movie-Data.csv")
+movie.head()
+movie.tail(2)
+map(lambda *a: a, range(3))
